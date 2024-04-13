70ebe918c8dac1589d7464fa73e39959a8f5cf47
rgunasekaran003 <guna1122k3@gmail.com>
2024-04-13 16:24:34 +0530

    created ball and outer boundary

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ canvas.pack()
 canvas.create_line(225, 0, 225, 400)
 canvas.create_line(0, 200, 450, 200)
 
+radius = 150 #set the arc radius
+canvas_middle = [int(canvas['width'])/2, int(canvas['height'])/2] #find the middle of the canvas
+canvas.create_oval(canvas_middle[0] - radius, canvas_middle[1] - radius, canvas_middle[0] + radius, canvas_middle[1] + radius, width = 3)
+
+ballradius = 20
+canvas.create_oval
+canvas.create_oval(canvas_middle[0] - ballradius, canvas_middle[1] - ballradius, canvas_middle[0] + ballradius, canvas_middle[1] + ballradius, fill="blue")
+
 # application loop
 win.mainloop()
