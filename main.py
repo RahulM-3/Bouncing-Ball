09fd86ea4e18472d2eb8403fc6e6b3e1d736f5c9
RahulM-3 <rahulcode97@gmail.com>
2024-04-13 16:46:22 +0530

    Gravity added

--- main.py
+++ main.py
@@ -5,19 +5,40 @@ win = tkinter.Tk()
 win.title("Bouncing Ball")
 win.geometry("450x400")
 
-# X and Y axis
+# Canvas
 canvas = tkinter.Canvas(win, width=450, height=400)
 canvas.pack()
+#find the middle of the canvas
+canvas_middle = [int(canvas['width'])/2, int(canvas['height'])/2]
+
+# X and Y axis
 canvas.create_line(225, 0, 225, 400)
 canvas.create_line(0, 200, 450, 200)
 
-radius = 150 #set the arc radius
-canvas_middle = [int(canvas['width'])/2, int(canvas['height'])/2] #find the middle of the canvas
-canvas.create_oval(canvas_middle[0] - radius, canvas_middle[1] - radius, canvas_middle[0] + radius, canvas_middle[1] + radius, width = 3)
+# Outer Boundary
+radius = 150
+canvas.create_oval(canvas_middle[0] - radius, canvas_middle[1] - radius, canvas_middle[0] + radius, canvas_middle[1] + radius, width=3)
 
+# Ball
+attitude = canvas_middle
 ballradius = 20
-canvas.create_oval
-canvas.create_oval(canvas_middle[0] - ballradius, canvas_middle[1] - ballradius, canvas_middle[0] + ballradius, canvas_middle[1] + ballradius, fill="blue")
+x0 = attitude[0]-ballradius
+y0 = attitude[1]-ballradius
+x1 = attitude[0]+ballradius
+y1 = attitude[1]+ballradius
+ball = canvas.create_oval(x0, y0, x1, y1, fill="blue")
+
+# Gravity
+def gravity():
+    attitude[1] += 9.807
+    x0 = attitude[0]-ballradius
+    y0 = attitude[1]-ballradius
+    x1 = attitude[0]+ballradius
+    y1 = attitude[1]+ballradius
+    canvas.coords(ball, x0, y0, x1, y1)
+    win.after(100, gravity, )
+
+gravity()
 
 # application loop
 win.mainloop()
